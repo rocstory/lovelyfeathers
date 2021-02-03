3be783bba2df5b27a3fcd8639cfcaf4179cf851d
Malik R <maliktroc@outlook.com>
2021-02-03 17:41:51 -0500

    home menu impl.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import HomeMenu from './components/HomeMenu/HomeMenu';
 
 function App(props)
 {
   return (
     <div className="App">
+      <HomeMenu />
+      {/* <div className="footer">
+        Testing
+      </div> */}
     </div>
   )
 }
---
+++ src/components/HomeMenu/HomeMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomeMenu.css';
+
+function HomeMenu(props)
+{
+  return (
+    <div className="home-menu menu gray-paper">
+        <h2 className="prj-title">Lovely Feathers</h2>
+        <h3 className="slogan">Always be kind</h3>
+    </div>
+  )
+}
+export default HomeMenu;
